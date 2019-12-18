81a6b5ccc518472d6de2215c2d24ef503308c950
David Holmes <phoenix1988@gmail.com>
2019-12-18 15:00:42 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,15 +3,17 @@ def create_an_empty_array
 end
 
 def create_an_array
-
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  array = [1, 2, 3]
+  array << 4
 end
 
 def add_element_to_start_of_array(array, element)
-
+  array = [1, 2, 3]
+  array.unshift(0)
 end
 
 def remove_element_from_end_of_array(array)
